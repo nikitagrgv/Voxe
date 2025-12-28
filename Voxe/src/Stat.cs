9ddc8b346b6a6ae3edbe102a4de9ea4815d6b428
Nikita Grigorev <pettyaelse@gmail.com>
2025-12-29 01:24:37 +0700

    w

--- Voxe/src/Stat.cs
+++ Voxe/src/Stat.cs
@@ -5,6 +5,11 @@ public static class Stat
 	public static ulong RenderedIndices { get; set; }
 	public static ulong RenderedIndicesPerFrame { get; set; }
 
+	public static void EndFrame()
+	{
+		RenderedIndicesPerFrame = 0;
+	}
+
 	public static void AddRenderedIndices(ulong renderedIndices)
 	{
 		RenderedIndices += renderedIndices;
