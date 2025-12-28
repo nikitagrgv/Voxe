e4e20af1efc48a02698c02893b001e23333f1274
Nikita Grigorev <pettyaelse@gmail.com>
2025-12-29 00:52:13 +0700

    w

--- Voxe/src/World.cs
+++ Voxe/src/World.cs
@@ -6,6 +6,16 @@ public class World
 {
 	private readonly Dictionary<ChunkIndex, Chunk> _chunksMap = new();
 
+	public readonly ref struct ChunksList
+	{
+		public ChunksList(ReadOnlySpan<Chunk> chunks)
+		{
+			Chunks = chunks;
+		}
+
+		public ReadOnlySpan<Chunk> Chunks { get; }
+	}
+
 	public void InitChunk(ChunkIndex index, Chunk chunk)
 	{
 		Debug.Assert(TryGetChunk(index) == null);
