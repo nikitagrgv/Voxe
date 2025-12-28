9f2186a7a52b5b545d7a1bb078ff3f487175d897
Nikita Grigorev <pettyaelse@gmail.com>
2025-12-28 23:00:03 +0700

    w

--- Voxe/src/MainWindow.cs
+++ Voxe/src/MainWindow.cs
@@ -169,8 +169,11 @@ public class MainWindow : NativeWindow
 		_camera.Position = new Vector3(0, 0, 8);
 
 		SimplexPerlin perlin = new(seed: 1234, NoiseQuality.Best);
-		NoiseMap heightMap = new(Chunk.ChunkWidth, Chunk.ChunkWidth);
+		NoiseMap heightMap = new();
 		NoiseMapBuilderPlane heightMapBuilder = new(0, 0, 1, 1, true);
+		heightMapBuilder.SourceModule = perlin;
+		heightMapBuilder.NoiseMap = heightMap;
+		heightMapBuilder.SetSize(Chunk.ChunkWidth, Chunk.ChunkWidth);
 
 		Chunk chunk = new();
 		for (int y = 0; y < Chunk.ChunkHeight; y++)
