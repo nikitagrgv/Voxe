c4f8b1f13309e048f10a58f5fb6eea652e1d5d72
Nikita Grigorev <pettyaelse@gmail.com>
2025-12-29 01:14:46 +0700

    w

--- Voxe/src/MainWindow.cs
+++ Voxe/src/MainWindow.cs
@@ -274,7 +274,7 @@ public class MainWindow : NativeWindow
 					Vector3 chunkPos = VoxelUtils.GetChunkPosition(index.X, index.Z);
 					Matrix4 chunksModel = Matrix4.CreateTranslation(chunkPos);
 					Matrix4 chunkMvp = chunksModel * viewProj;
-					shader.SetUniform(mvpLocation, mvp);
+					shader.SetUniform(mvpLocation, chunkMvp);
 					chunk.Mesh.Render();
 				}
 			}
