f0a478dd14de5484d819031af93aeeee5798cf83
Nikita Grigorev <pettyaelse@gmail.com>
2025-12-29 00:48:49 +0700

    w

--- Voxe/src/World.cs
+++ Voxe/src/World.cs
@@ -4,6 +4,9 @@ public class World
 {
 	private Dictionary<ChunkIndex, Chunk> _chunksMap = new();
 
-	
-	
+	public Chunk? TryGetChunk(ChunkIndex chunkIndex)
+	{
+		_chunksMap.TryGetValue(chunkIndex, out Chunk? chunk);
+		return chunk;
+	}
 }
