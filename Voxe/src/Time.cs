224658dc72616db1334c704a346449a04cb25922
Nikita Grigorev <pettyaelse@gmail.com>
2025-12-28 20:29:04 +0700

    w

--- Voxe/src/Time.cs
+++ Voxe/src/Time.cs
@@ -10,12 +10,12 @@ public static class Time
 	private static double _deltaTime = 0;
 	private static double _fps = 0;
 
-	public static TimeSpan CurrentTime { get; internal set; }
+	public static TimeSpan CurrentTime { get; private set; }
 
 	public static TimeSpan DeltaSpan
 	{
 		get;
-		internal set
+		private set
 		{
 			_deltaTime = value.TotalSeconds;
 			_fps = 1 / _deltaTime;
@@ -25,4 +25,10 @@ public static class Time
 
 	public static double DeltaTime => _deltaTime;
 	public static double Fps => _fps;
+
+	internal static void UpdateTime(TimeSpan currentTime, TimeSpan delta)
+	{
+		CurrentTime = currentTime;
+		DeltaSpan = delta;
+	}
 }
