c541db2689bbefa050baa3d0ba75b4fcdc60c48d
Nikita Grigorev <pettyaelse@gmail.com>
2025-12-28 23:13:34 +0700

    w

--- Voxe/src/MainWindow.cs
+++ Voxe/src/MainWindow.cs
@@ -181,13 +181,21 @@ public class MainWindow : NativeWindow
 		Chunk chunk = new();
 		for (int y = 0; y < Chunk.ChunkHeight; y++)
 		{
+			float heightNormalized = heightMap.GetValue(x, z);
+			float height = heightNormalized * Chunk.ChunkHeight;
+			int h = (int)height;
+			Block block;
+			if (y > h)
+				block = new Block(BasicBlock.Air);
+			else if (y == h)
+				block = new Block(BasicBlock.Grass);
+			else
+				block = new Block(BasicBlock.Dirt);
+
 			for (int z = 0; z < Chunk.ChunkWidth; z++)
 			{
 				for (int x = 0; x < Chunk.ChunkWidth; x++)
 				{
-					float heightNormalized = heightMap.GetValue(x, z);
-					float height = heightNormalized * Chunk.ChunkHeight;
-					Block block = y > 5 ? new Block(BasicBlock.Snow) : new Block(BasicBlock.Grass);
 					chunk.SetBlock(x, y, z, block);
 				}
 			}
