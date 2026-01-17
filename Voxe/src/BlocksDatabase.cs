f8a32f15378e24d90b5dce344da965cb82d231f0
Nikita Grigorev <pettyaelse@gmail.com>
2026-01-17 19:02:51 +0700

    w

--- Voxe/src/BlocksDatabase.cs
+++ Voxe/src/BlocksDatabase.cs
@@ -1,22 +1,44 @@
-﻿namespace Voxe;
+﻿using System.Text.Json.Serialization;
+
+namespace Voxe;
 
 public class BlocksDatabase
 {
-	public struct Block
+	public readonly struct Block
 	{
-		public int Id { get; set; }
-		public string Name { get; set; }
-		public bool IsTransparent { get; set; }
-		public bool IsInvisible { get; set; }
+		public ushort Id { get; }
+		public string Name { get; }
+		public bool IsTransparent { get; }
+		public bool IsInvisible { get; }
 
-		public Image? ImagePX { get; set; }
-		public Image? ImageNX { get; set; }
+		public Image? ImagePX { get; }
+		public Image? ImageNX { get; }
 
-		public Image? ImagePY { get; set; }
-		public Image? ImageNY { get; set; }
+		public Image? ImagePY { get; }
+		public Image? ImageNY { get; }
 
-		public Image? ImagePZ { get; set; }
-		public Image? ImageNZ { get; set; }
+		public Image? ImagePZ { get; }
+		public Image? ImageNZ { get; }
+	}
+
+	private readonly struct ParsedBlock
+	{
+		[JsonPropertyName("id")] public ushort Id { get; }
+		[JsonPropertyName("name")] public string Name { get; }
+
+		[JsonPropertyName("transparent")] public bool? IsTransparent { get; }
+		[JsonPropertyName("invisible")] public bool? IsInvisible { get; }
+
+		[JsonPropertyName("texture")] public string? TexturePath { get; }
+
+		[JsonPropertyName("texture-px")] public string? TexturePathPX { get; }
+		[JsonPropertyName("texture-nx")] public string? TexturePathNX { get; }
+
+		[JsonPropertyName("texture-py")] public string? TexturePathPY { get; }
+		[JsonPropertyName("texture-ny")] public string? TexturePathNY { get; }
+
+		[JsonPropertyName("texture-pz")] public string? TexturePathPZ { get; }
+		[JsonPropertyName("texture-nz")] public string? TexturePathNZ { get; }
 	}
 
 	public Block[] Parse(string databaseRelPath)
