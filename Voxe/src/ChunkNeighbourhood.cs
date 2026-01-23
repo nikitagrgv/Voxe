bcd49d2376ac7237cbdb77e7db05b1ecd7fda959
Nikita Grigorev <nikitagrgv@unigine.com>
2026-01-23 20:21:53 +0700

    w

---
+++ Voxe/src/ChunkNeighbourhood.cs
@@ -0,0 +1,17 @@
+﻿namespace Voxe;
+
+public readonly struct ChunkNeighbourhood
+{
+	public Chunk PositiveX { get; init; }
+	public Chunk NegativeX { get; init; }
+	public Chunk PositiveZ { get; init; }
+	public Chunk NegativeZ { get; init; }
+
+	public ChunkNeighbourhood(Chunk positiveX, Chunk negativeX, Chunk positiveZ, Chunk negativeZ)
+	{
+		PositiveX = positiveX;
+		NegativeX = negativeX;
+		PositiveZ = positiveZ;
+		NegativeZ = negativeZ;
+	}
+}
